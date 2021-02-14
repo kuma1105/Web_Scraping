42ec3cd11ec4f54853e7ebf0619b94074fc2dd14
Fancyee <74527909+kuma1105@users.noreply.github.com>
2021-02-15 01:16:46 +0900

    Add files via upload

---
+++ deliciousSoup_2.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from urllib.request import HTTPError
+from urllib.request import urlopen
+
+html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('http://www.pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
+
